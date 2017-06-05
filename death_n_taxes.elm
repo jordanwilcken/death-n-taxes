f061f9afe1d7ecd2daffb64329bcfdc6fdece428
Jordan Wilcken <jwwilcken@verisk.com>
2017-06-05 22:23:01 +0000

    crawling along

--- death_n_taxes.elm
+++ death_n_taxes.elm
@@ -1,26 +1,36 @@
 import Html exposing (..)
 import Html.Events exposing (onClick)
 import Html.Attributes exposing (..)
+import List
 
 main =
-  beginnerProgram { model = 0, view = view, update = update }
+  beginnerProgram { model = model, view = view, update = update }
 
+type alias Model = { years: (List Int) }
+
+model =
+  Model [2007]
 
 view model =
-  table []
-    [ tr [] [ th [] [ text "year"], th [] [text "federal income tax paid" ] ]
-    , tr [] [ td [] [ input [] [] ], td [] [ input [] [] ] ]
+  div []
+    [ table []
+      [ tr [] [ th [] [ text "year"], th [] [text "federal income tax paid" ] ]
+      , tr [] [ td [] [ input [] [] ], td [] [ input [] [] ] ]
+      ]
+    , button [] [ text "+" ]
+    , p [] [ text "these are the stats: 21%" ]
     ]
-  button [] [ text "+" ]
-  p [] [ text "these are the stats: 21%" ]
+
+row : Int -> Html msg
+row year =
+  tr [] [ td[] [ input [value (toString year)] [] ], td [] [ input [] [ ] ] ]
 
 type Msg = Increment | Decrement
-
 
 update msg model =
   case msg of
     Increment ->
-      model + 1
+      { model | years = 1 :: model.years }
 
     Decrement ->
-      model - 1
+      { model | years = 1 :: model.years }
