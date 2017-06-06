ce573582353516c43bf722bdb0a82d96e026e5e1
Jordan Wilcken <jwwilcken@verisk.com>
2017-06-06 22:32:29 +0000

    some progress

--- death_n_taxes.elm
+++ death_n_taxes.elm
@@ -1,7 +1,7 @@
 import Html exposing (..)
 import Html.Events exposing (onClick)
 import Html.Attributes exposing (..)
-import List
+import List exposing(..)
 
 main =
   beginnerProgram { model = model, view = view, update = update }
@@ -13,10 +13,7 @@ model =
 
 view model =
   div []
-    [ table []
-      [ tr [] [ th [] [ text "year"], th [] [text "federal income tax paid" ] ]
-      , tr [] [ td [] [ input [] [] ], td [] [ input [] [] ] ]
-      ]
+    [ table [] (makeTableRows makeHeader makeRows model.years)
     , button [] [ text "+" ]
     , p [] [ text "these are the stats: 21%" ]
     ]
@@ -24,6 +21,22 @@ view model =
 row : Int -> Html msg
 row year =
   tr [] [ td[] [ input [value (toString year)] [] ], td [] [ input [] [ ] ] ]
+
+makeTableRows : Html msg -> ((List Int) -> (List (Html msg))) -> List Int -> (List (Html msg))
+makeTableRows makeHeaderFun makeRowsFun years=
+  makeHeaderFun :: makeRowsFun years
+  
+makeHeader : Html msg
+makeHeader =
+  tr [] [ th [] [ text "year"], th [] [text "federal income tax paid" ] ]
+   
+makeRows : (List Int) -> List (Html msg)
+makeRows years =
+  List.map (\year -> makeOneRow year) years
+
+makeOneRow : Int -> Html msg
+makeOneRow year =
+  tr [] [ td [] [ input [] [] ], td [] [ input [] [] ] ]
 
 type Msg = Increment | Decrement
 
